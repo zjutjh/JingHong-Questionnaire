1d4b0b76628d14f124dde9dd67693dd81ca05513
RosyrRais <syr050301@qq.com>
2024-05-03 15:53:21 +0800

    chore(UIs): 导入了elementUI和NaiveUI

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import { createApp } from 'vue'
 import './style.css'
 import router from './router';
 import pinia from './stores/createPinia';
-import App from './App.vue'
+import naive from 'naive-ui';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import App from './App.vue';
 
 const app = createApp(App);
 app.use(router);
 app.use(pinia);
+app.use(naive);
+app.use(ElementPlus);
 app.mount("#app");
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,4 +2,9 @@
   <div class="text-red-300">
     indexPage
   </div>
+  <n-button>Default</n-button>
+  <el-button>Default</el-button>
 </template>
+
+<script setup lang="ts">
+</script>
