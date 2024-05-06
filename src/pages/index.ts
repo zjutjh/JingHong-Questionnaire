e9ca75216baeb3a54f7fc85e41b9fb6dfb8891a8
xixiibn5100 <1584914306@qq.com>
2024-05-06 21:11:32 +0800

    feat(DetailInfo):初步完成编辑问卷的侧边栏,增加路由跳转动画效果

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,9 @@
 import home from "./home.vue";
-import login from './Login/index.vue'
+import login from './Login/index.vue';
+import detailInfo from './DetailInfo/index.vue';
+
 export {
   home,
-  login
+  login,
+  detailInfo,
 }
