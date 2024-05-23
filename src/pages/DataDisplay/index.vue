b78fddbc43a15fe0462c8e3437cb8e34abd8b846
RosyrRais <syr050301@qq.com>
2024-05-23 18:27:27 +0800

    feat(dataDisplay): 将表格功能完善了

--- src/pages/DataDisplay/index.vue
+++ src/pages/DataDisplay/index.vue
@@ -12,23 +12,22 @@
     </div>
   </div>
   <div class="overflow-x-auto">
-    <!-- <table class="table">
+    <table class="table">
       <thead>
         <tr>
-          <th></th>
           <th>序号</th>
           <th>时间</th>
-          <th>etc</th>
+          <th v-for="ans in answers">{{ ans.title }}</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(ans, index) in answers.question_answers">
-          <th>{{ index }}</th>
-          <td>{{ answers.time[index] }}</td>
-          <td></td>
+        <tr v-for="(t, index) in time">
+          <th>{{ index+1 }}</th>
+          <th>{{ t }}</th>
+          <th v-for="ans in answers">{{ ans.answers[index] }}</th>
         </tr>
       </tbody>
-    </table> -->
+    </table>
   </div>
 </div>
 </template>
@@ -39,20 +38,25 @@ import { ref } from 'vue';
 import { useRequest } from 'vue-hooks-plus';
 import router from '@/router';
 
-const id = ref();
-const title = ref();
+const id = ref(15);
+const title = ref("Tiele");
+const pageNum = ref(1);
+const totalPageNum = ref(1);
 const answers = ref();
+const time = ref();
 
 const getAnswers = () => {
   useRequest(() => getAnswersAPI({
-    id: 13,
-    page_num: 5,
-    page_size: 1,
+    id: id.value,
+    page_num: pageNum.value,
+    page_size: 10,
   }), {
     onSuccess(res: any) {
       console.log(res);
       if(res.code === 200) {
-        answers.value = res.data;
+        totalPageNum.value = res.data.total_page_num;
+        answers.value = res.data.answers_data.question_answers;
+        time.value = res.data.answers_data.time;
       }
     }
   })
