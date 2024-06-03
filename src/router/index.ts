180076a3a474a8a031cd7dc5e26d186384473d75
RosyrRais <syr050301@qq.com>
2024-06-04 00:26:29 +0800

    feat(router): 新增了路由守卫

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,8 +5,8 @@ home,
 detailInfo,
 datadisplay,
 } from "@/pages";
-// import pinia from "@/stores/createPinia";
-// import { useMainStore } from "@/stores";
+import pinia from "@/stores/createPinia";
+import { useMainStore } from "@/stores";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -34,8 +34,13 @@ const router = createRouter({
   ]
 });
 
-// router.beforeEach((to, _, next) => {
-//   const loginStore = useMainStore(pinia).useLoginStore(pinia);
-// });
+router.beforeEach((to, _, next) => {
+  const loginStore = useMainStore(pinia).useLoginStore(pinia);
+  if(to.path !== "/login" && !loginStore.loginSession) {
+    next("/login");
+  } else {
+    next();
+  }
+});
 
 export default router;
