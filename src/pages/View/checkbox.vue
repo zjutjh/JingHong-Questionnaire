2ac09217d4dec3d39b768c5ab707ecd8a23b81ea
xixiibn5100 <1584914306@qq.com>
2024-08-02 20:37:25 +0800

    feat(submit):新增客户端多选题逻辑

--- src/pages/View/checkbox.vue
+++ src/pages/View/checkbox.vue
@@ -1,22 +1,22 @@
 <template>
-  <div class="bg-blue-100 p-30">
+  <div class="bg-blue-100 p-30 my-10">
     <div class="flex justify-between">
       <div class="flex-col">
         <div class="flex items-center gap-20">
           <span class="text-xl">{{ props.serial_num }}</span>
-          <span class="text-xl flex gap-5 items-center" >{{ props.title }} <el-tag type="primary" class="ml-5">多选</el-tag> <el-tag type="warning" v-if="localOptionChoose">选答</el-tag> <el-tag type="danger" v-if="localUnique">唯一</el-tag></span>
+          <span class="text-xl flex gap-5 items-center" >{{ props.title }} <el-tag type="primary" class="ml-5">多选</el-tag> <el-tag type="warning" v-if="!required">选答</el-tag> <el-tag type="danger" v-if="localUnique">唯一</el-tag></span>
         </div>
-        <div class="flex items-center mt-15 ml-10" >
+        <div class="flex items-center mt-15 ml-10">
           <span class="text-md text-gray-500">{{ props.describe }}</span>
         </div>
       </div>
-      <div class="flex-col justify-center items-center ">
+      <div class="flex-col justify-center items-center">
       </div>
     </div>
     <div class="divider my-5"></div>
-    <div class="flex-col p-5 h-auto ">
-      <div v-for="(item,index) in localOptions" :key="item.serial_num" class="flex items-center gap-10 my-5">
-        <input type="checkbox" :name="props.serial_num" class="checkbox-sm my-5" :value="item.content"  v-model="answerArr[index]"/>
+    <div class="flex-col p-5 h-auto">
+      <div v-for="item in localOptions" :key="item.serial_num" class="flex items-center gap-10 my-5">
+        <input type="checkbox" :name="props.serial_num" class="checkbox-sm my-5" :value="item.content" v-model="answerArr"/>
         <span v-if="item.content">{{ item.content }}</span>
         <div class="ml-10 flex items-center gap-20">
           <div v-if="item.img" class="mt-4">
@@ -25,7 +25,7 @@
         </div>
       </div>
       <div class="flex gap-10 mt-10" v-if="localOtherOption">
-        <input type="checkbox" :name="props.serial_num" class="checkbox-sm my-5" :value="otherAnswer"/>
+        <input type="checkbox" :name="props.serial_num" class="checkbox-sm my-5" :value="otherAnswer" v-model="answerArr"/>
         <input type="text" class="input-sm w-100 rounded-xl" placeholder="其他" v-model="otherAnswer"/>
       </div>
     </div>
@@ -33,19 +33,16 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch, defineProps, defineEmits, toRaw, nextTick } from 'vue';
-import { useRequest } from "vue-hooks-plus";
-import { saveImgAPI } from "@/apis";
-import {ElNotification} from "element-plus";
+import { ref, watch, defineProps, defineEmits } from 'vue';
 
 const props = defineProps<{
   serial_num: number,
   title?: string,
-  optionChoose: boolean,
+  required: boolean,
   unique: boolean,
   otherOption: boolean,
   describe: string,
-  type: string,
+  answer: string,
   options?: {
     content: string;
     img: string;
@@ -53,16 +50,23 @@ const props = defineProps<{
   }[]
 }>();
 
-const emits = defineEmits();
-
-const localOptionChoose = ref<boolean>(props.optionChoose);
 const localUnique = ref<boolean>(props.unique);
 const localOtherOption = ref<boolean>(props.otherOption);
 const localOptions = ref(props.options ? [...props.options] : []);
-const answerArr = ref<string[]>([]);
 const otherAnswer = ref<string>('');
+const answerArr = ref<string[]>([]);
 
+const emits = defineEmits(['update:answer']);
 
+watch([answerArr, otherAnswer], ([newAnswerArr, newOtherAnswer]) => {
+  if (localOtherOption.value && newOtherAnswer && newAnswerArr.includes(newOtherAnswer)) {
+    const combinedAnswers = newAnswerArr.filter(answer => answer !== newOtherAnswer).concat(newOtherAnswer).join('┋');
+    emits('update:answer', combinedAnswers);
+  } else {
+    const combinedAnswers = newAnswerArr.join('|');
+    emits('update:answer', combinedAnswers);
+  }
+});
 </script>
 
 <style scoped>
