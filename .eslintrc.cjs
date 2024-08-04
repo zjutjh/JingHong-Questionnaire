a8a36baa5ec6577e72a4a69ab979b881e3206efc
xixiibn5100 <1584914306@qq.com>
2024-08-05 01:28:24 +0800

    feat:新增eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    "vue/multi-word-component-names": 0,
+    "vue/require-v-for-key": 0
+  }
+}
