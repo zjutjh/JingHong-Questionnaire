356bf8ff9e786947f4dbd65da5e533e54a5a897e
xixiibn5100 <1584914306@qq.com>
2024-08-19 21:26:24 +0800

    fix(QA): [text] 减小字体大小,适配移动端

--- src/pages/View/checkbox.vue
+++ src/pages/View/checkbox.vue
@@ -3,11 +3,11 @@
     <div class="flex justify-between">
       <div class="flex-col">
         <div class="flex items-center gap-20">
-          <span class="text-xl">{{ props.serial_num }}</span>
-          <span class="text-xl flex gap-5 items-center" >{{ props.title }} <el-tag type="primary" class="ml-5">多选</el-tag> <el-tag type="warning" v-if="!required">选答</el-tag> <el-tag type="danger" v-if="localUnique">唯一</el-tag></span>
+          <span class="lg:text-xl md:text-md">{{ props.serial_num }}</span>
+          <span class="lg:text-xl md:text-md flex gap-5 items-center" >{{ props.title }} <el-tag type="primary" class="ml-5">多选</el-tag> <el-tag type="warning" v-if="!required">选答</el-tag> <el-tag type="danger" v-if="localUnique">唯一</el-tag></span>
         </div>
         <div class="flex items-center mt-15 ml-10">
-          <span class="text-md text-gray-500">{{ props.describe }}</span>
+          <span class="text-sm text-gray-500">{{ props.describe }}</span>
         </div>
       </div>
       <div class="flex-col justify-center items-center">
@@ -17,7 +17,7 @@
     <div class="flex-col p-5 h-auto">
       <div v-for="item in localOptions" :key="item.serial_num" class="flex items-center gap-10 my-5">
         <input type="checkbox" :name="props.serial_num" class="my-5" style="zoom: 140%" :value="item.content" v-model="answerArr"/>
-        <span v-if="item.content">{{ item.content }}</span>
+        <span v-if="item.content" class="text-sm">{{ item.content }}</span>
         <div class="ml-10 flex items-center gap-20">
           <div v-if="item.img" class="mt-4">
             <img v-if="item.img" :src="item.img" alt="Preview" style="max-width: 150px; max-height: 150px;" />
@@ -25,7 +25,7 @@
         </div>
       </div>
       <div class="flex gap-10 mt-10" v-if="localOtherOption">
-        <input type="checkbox" :name="props.serial_num" class="checkbox-sm my-5" :value="otherAnswer" id='other'  @click='otherAnswerChecked = !otherAnswerChecked'/>
+        <input type="checkbox" :name="props.serial_num" class="my-5" style="zoom: 140%" :value="otherAnswer" id='other'  @click='otherAnswerChecked = !otherAnswerChecked'/>
         <input type="text" class="input-sm w-150" placeholder="其他" v-model="otherAnswer" @input="updateOtherAnswer"  />
       </div>
     </div>
