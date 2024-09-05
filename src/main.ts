fefbb44a37dc68052dd6d4b95dcc0e3939f288c3
xixiibn5100 <1584914306@qq.com>
2024-09-05 13:12:58 +0800

    fix:修复了上传脚本失败的bug

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import router from './router'
 import pinia from './stores/createPinia'
-import naive from 'naive-ui'
-import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
@@ -14,6 +12,4 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 }
 app.use(router)
 app.use(pinia)
-app.use(naive)
-app.use(ElementPlus)
 app.mount('#app')
