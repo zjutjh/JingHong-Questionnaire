26b0f02280e0a20f9243b9318cfb6921568381e8
xixiibn5100 <1584914306@qq.com>
2024-10-02 02:50:38 +0800

    perf: 按需导入elmUI

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,33 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {ElementPlusResolver} from "unplugin-vue-components/resolvers";
 
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  server:{
-    proxy:{
-      '/api':{
-        target:"https://qa.qianqianzyk.top",
-        changeOrigin: true,
-        rewrite:(path) => path.replace(/^\/api/,'')
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+
+    ],
+    server: {
+      proxy: {
+        "/api": env.HOST
+      }
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
     }
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
   }
+
 })
