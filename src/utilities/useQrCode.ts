dca8f889c844799690bcf940f95e1e304e5cd922
maojianhui <3605828735@qq.com>
2024-10-14 11:47:05 +0800

    feat: 新增问卷二维码分享

---
+++ src/utilities/useQrCode.ts
@@ -0,0 +1,51 @@
+import { ref, onMounted } from 'vue'
+import QRCode from 'qrcode';
+import { ElMessage } from 'element-plus';
+import { showModal } from '@/components';
+
+export const useQrCode = (url: string) => {
+    const qrCodeURL = ref(''); // 用于存储二维码的 base64 URL
+    //复制二维码到剪贴板
+    const generateQRCode = async () => {
+        try {
+            qrCodeURL.value = await QRCode.toDataURL(url, {
+                width: 300,
+                margin: 2,
+                color: {
+                    dark: '#000000',
+                    light: '#FFFFFF',
+                },
+            });
+        } catch (error) {
+            console.error('二维码生成失败:', error);
+        }
+    };
+
+    onMounted(() => {
+        generateQRCode();
+    });
+
+    //复制二维码到剪贴板
+    const copyQrCode = async () => {
+        try {
+            const response = await fetch(qrCodeURL.value)
+            const blob = await response.blob()
+            const tempItem = new ClipboardItem({ [blob.type]: blob });
+            await navigator.clipboard.write([tempItem])
+            showModal('QRcode', true);
+            ElMessage({
+                message: '二维码复制成功',
+                type: 'success',
+            })
+        } catch {
+            ElMessage({
+                message: '二维码复制失败',
+                type: 'error',
+            })
+        }
+    }
+    return {
+        qrCodeURL,
+        copyQrCode
+    }
+}
