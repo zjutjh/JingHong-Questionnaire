5e4da456f3b0780d49e2b9bcc122734549aa686c
xixiibn5100 <1584914306@qq.com>
2025-02-17 22:16:19 +0800

    refactor(render): split verify modal

--- src/pages/View/mainList.vue
+++ src/pages/View/mainList.vue
@@ -8,7 +8,7 @@
       :result-data="props.resultData"
     />
     <div class="flex justify-center items-center py-50">
-      <button v-if="props.decryptedId !== '' && !isOutDate" class="btn  w-1/3 bg-red-800 text-red-50 dark:opacity-75 hover:bg-red-600" @click="showModal('QuestionnaireSubmit')">
+      <button v-if="props.decryptedId !== '' && !isOutDate" class="btn  w-1/3 bg-red-800 text-red-50 dark:opacity-75 hover:bg-red-600" @click="handleSubmit">
         提交
       </button>
     </div>
@@ -17,15 +17,27 @@
         <span class="text-red-950 dark:text-red-500 ">提交问卷</span>
       </template>
 
-      <template v-if="formData && !formData.verify || tokenOutDate" #default>
+      <template #default>
         你确认要提交问卷吗?
       </template>
-      <template v-else #default>
+
+      <template #action>
+        <button
+          v-if="formData && !formData.verify || tokenOutDate"
+          class="btn bg-red-800 text-red-50 w-full hover:bg-red-600"
+          @click="submit"
+        >
+          确认
+        </button>
+      </template>
+    </modal>
+    <modal modal-id="QuestionnaireSubmitWithVerify">
+      <template #title>
+        统一身份认证
+      </template>
+      <template #default>
         <div class="flex-col">
-          <div class="text-sm">
-            该问卷仅限校内师生作答,提交前需要先进行<span class="font-bold">统一身份认证</span>
-          </div>
-          <div class="flex-col my-10">
+          <div class="flex-col ">
             <span>职工号/学号 <input v-model="verifyData.stu_id" class="dark:bg-customGray_more_shallow input input-bordered shadow-md h-35 my-10" style="width: 100%"></span><br>
             <span>密码 <br><input
               v-model="verifyData.password"
@@ -34,8 +46,11 @@
               type="password"
             ></span>
           </div>
-          <div class="flex justify-end">
-            <a href="https://oauth.zjut.edu.cn/im/V3/securitycenter/findPwd/index.zf" style=" text-decoration: underline;;" class="text-sm my-5 text-blue-500 dark:text-white">
+          <div class="flex justify-between">
+            <div class="text-[12px]">
+              问卷限校内作答,请进行<span class="font-bold">统一身份认证</span>
+            </div>
+            <a href="https://oauth.zjut.edu.cn/im/V3/securitycenter/findPwd/index.zf" style=" text-decoration: underline;" class="text-sm my-5 text-blue-500 dark:text-white">
               忘记密码?
             </a>
           </div>
@@ -43,17 +58,7 @@
       </template>
       <template #action>
         <button
-          v-if="formData && !formData.verify || tokenOutDate"
-          class="btn bg-red-800 text-red-50 w-full hover:bg-red-600"
-          style="margin-top: -10px"
-          @click="submit"
-        >
-          确认
-        </button>
-        <button
-          v-else
-          class="btn bg-red-800 text-red-50 w-full hover:bg-red-600"
-          style="margin-top: -10px"
+          class="btn bg-red-800 text-red-50 w-full hover:bg-red-600 mt-[-20px]"
           @click="verify"
         >
           确认
@@ -66,9 +71,20 @@
 import QuesList from "@/pages/View/quesList.vue";
 import VoteList from "@/pages/View/voteList.vue";
 import { showModal, modal } from "@/components";
-import { ref } from "vue";
+import { computed, ref } from "vue";
+import { useRequest } from "vue-hooks-plus";
+import { ElNotification } from "element-plus";
+import verifyAPI from "@/apis/service/User/verifyApi.ts";
+import { setUserSubmitAPI } from "@/apis";
+import { closeLoading, startLoading } from "@/utilities";
+import { useMainStore } from "@/stores";
+import router from "@/router";
+import getStatistic from "@/apis/service/User/getStatistic.ts";
 const allowSend = ref(true);
 const isOutDate = ref(false);
+const optionStore = useMainStore().useOptionStore();
+const questionnaireStore = useMainStore().useQuetionnaireStore();
+
 const verifyData = ref({
   stu_id: "",
   password: "",
@@ -79,7 +95,134 @@ const props = defineProps<{
   question: any
   decryptedId: any
   resultData?: any
+  submitData: any
 }>();
+const submitData = ref({
+  id: props.decryptedId,
+  questions_list: [],
+  token: ""
+});
+
+const handleSubmit = () => {
+  const nowDate = Date.now();
+  const startTimestamp = new Date(props.formData.start_time).getTime();
+  const showTime = props.formData.start_time.replace("T", " ").split("+")[0].split(".")[0];
+  if (nowDate - startTimestamp < 0) {
+    ElNotification.error(`问卷开始时间为 ${showTime}`);
+  } else if (props.formData.verify) {
+    showModal("QuestionnaireSubmitWithVerify");
+  } else {
+    showModal("QuestionnaireSubmit");
+  }
+};
+const tokenOutDate = computed(() => {
+  const lastDate = localStorage.getItem("timestamp");
+  // 如果没有存储时间戳（首次请求或过期），调用 verifyAPI
+  return !(!lastDate || Date.now() - parseInt(lastDate) > 7 * 24 * 60 * 60 * 1000);
+});
+
+const verify = () => {
+  const lastDate = localStorage.getItem("timestamp");
+  // 如果没有存储时间戳（首次请求或过期），调用 verifyAPI
+  if (!lastDate || Date.now() - parseInt(lastDate) > 7 * 24 * 60 * 60 * 1000) {
+    // 调用 verifyAPI 获取新的 token
+    useRequest(() => verifyAPI(verifyData.value), {
+      onSuccess(res) {
+        if (res.code === 200) {
+          // 更新 token 和 timestamp
+          localStorage.setItem("token", res.data.token);
+          localStorage.setItem("timestamp", String(Date.now()));
+          submit();
+        } else {
+          ElNotification.error(res.msg);
+        }
+      }
+    });
+  } else {
+    // 如果 timestamp 存在且未过期，直接调用 submit
+    submit();
+  }
+};
+
+const submit = () => {
+  checkAnswer();
+  if (allowSend.value === false) {
+    return;
+  }
+  submitData.value.questions_list = props.question.map((q) => ({
+    question_id: q.id,
+    serial_num: q.serial_num,
+    answer: q.answer
+  }));
+  submitData.value.token = localStorage.getItem("token");
+  useRequest(() => setUserSubmitAPI(submitData.value), {
+    onBefore: () => startLoading(),
+    async onSuccess(res) {
+      if (res.code === 200 && res.msg === "OK") {
+        const imageStore = useMainStore().useImageStore();
+        ElNotification.success("提交成功");
+        questionnaireStore.deleteAnswer(props.decryptedId);
+        imageStore.clearFiles();
+        optionStore.deleteOption(props.decryptedId);
+        if (props.formData.survey_type === 0) {
+          router.push("/Thank");
+        // } else {
+        //   try {
+        //     const res = await getStatistic({ id: Number(decryptedId.value) });
+        //     resultData.value = res.data.statistics[0].options;
+        //   } catch (e) {
+        //     ElNotification.error(e);
+        //   }
+        }
+      } else {
+        ElNotification.error(res.msg);
+      }
+    },
+    onError(e) {
+      ElNotification.error(e.message);
+    },
+    onFinally: () => {
+      showModal("QuestionnaireSubmit", true);
+      closeLoading();
+    }
+  });
+};
+
+const checkAnswer = () => {
+  const hasUnansweredRequiredQuestion = props.question.some((q) => {
+    if (q.required && q.answer === "") {
+      ElNotification.error("您有题目未完成作答.");
+      return true;
+    }
+    if (q.question_type === 1 && q.answer === " ") {
+      ElNotification.error("您有单选题未完成作答.");
+      return true;
+    }
+    if (q.question_type === 2 && (q.answer.endsWith("┋") || q.answer.startsWith("┋"))) {
+      ElNotification.error("您有多选题未完成作答.");
+      return true;
+    }
+    if (q.question_type === 2 && q.answer.split("┋").length > q.maximum_option || q.answer.split("┋").length < q.minimum_option) {
+      if (q.answer.split("┋").length > q.maximum_option) {
+        ElNotification.error(`该投票最多只能选择${q.maximum_option}个选项`);
+      } else if (q.answer.split("┋").length < q.minimum_option) {
+        ElNotification.error(`该投票最少需要选择${q.maximum_option}个选项`);
+      }
+      return true;
+    }
+
+    if (q.question_type === 3 && q.answer !== "" && q.reg && !new RegExp(q.reg).test(q.answer)) {
+      ElNotification.error(`第${q.serial_num}题的回答不符合要求.`);
+      return true;
+    }
+  });
+  if (hasUnansweredRequiredQuestion) {
+    showModal("QuestionnaireSubmit", true);
+    allowSend.value = false;
+    return;
+  }
+  allowSend.value = true;
+};
 
 </script>
 <style scoped>
--- src/pages/View/view.vue
+++ src/pages/View/view.vue
@@ -16,18 +16,17 @@
         :form-data="formData"
         :question="question"
         :decrypted-id="decryptedId"
+        :submit-data="submitData"
       />
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { computed, onMounted, ref, watch } from "vue";
+import { onMounted, ref, watch } from "vue";
 import { useRequest } from "vue-hooks-plus";
-import { getUserAPI, setUserSubmitAPI } from "@/apis";
+import { getUserAPI } from "@/apis";
 import { ElNotification } from "element-plus";
-
-import router from "@/router";
 import { useRoute } from "vue-router";
 import { closeLoading, startLoading } from "@/utilities";
 import CryptoJS from "crypto-js";
@@ -50,66 +49,32 @@ const resultData = ref(undefined);
 const route = useRoute();
 const loginStore = useMainStore().useLoginStore();
 const decryptedId = ref<string | null>();
-const allowSend = ref(true);
 const isOutDate = ref(false);
 const verifyData = ref({
   stu_id: "",
   password: "",
   survey_id: -1
 });
-const optionStore = useMainStore().useOptionStore();
 const questionnaireStore = useMainStore().useQuetionnaireStore();
 onMounted(async () => {
-
   loginStore.setShowHeader(false);
   let idParam = route.query.id as string | undefined;
   if (idParam) {
-    // 解密 ID
     idParam = idParam.replace(/ /g, "+");
     decryptedId.value = decryptId(idParam) as string | null;
-    // console.log(decryptedId.value)
     verifyData.value.survey_id = Number(decryptedId.value);
     if (decryptedId.value === "") {
       ElNotification.error("无效的问卷id");
     }
   }
   getQuestionnaireView();
-  try {
-    const res = await getStatistic({ id: Number(decryptedId.value) });
-    resultData.value = res.data.statistics[0].options;
-  } catch (e) {
-    ElNotification.error(e);
-  }
+  // try {
+  //   const res = await getStatistic({ id: Number(decryptedId.value) });
+  //   resultData.value = res.data.statistics[0].options;
+  // } catch (e) {
+  //   ElNotification.error(e);
+  // }
 });
-
-const tokenOutDate = computed(() => {
-  const lastDate = localStorage.getItem("timestamp");
-  // 如果没有存储时间戳（首次请求或过期），调用 verifyAPI
-  return !(!lastDate || Date.now() - parseInt(lastDate) > 7 * 24 * 60 * 60 * 1000);
-});
-
-const verify = () => {
-  const lastDate = localStorage.getItem("timestamp");
-  // 如果没有存储时间戳（首次请求或过期），调用 verifyAPI
-  if (!lastDate || Date.now() - parseInt(lastDate) > 7 * 24 * 60 * 60 * 1000) {
-    // 调用 verifyAPI 获取新的 token
-    useRequest(() => verifyAPI(verifyData.value), {
-      onSuccess(res) {
-        if (res.code === 200) {
-          // 更新 token 和 timestamp
-          localStorage.setItem("token", res.data.token);
-          localStorage.setItem("timestamp", String(Date.now()));
-          submit();
-        } else {
-          ElNotification.error(res.msg);
-        }
-      }
-    });
-  } else {
-    // 如果 timestamp 存在且未过期，直接调用 submit
-    submit();
-  }
-};
 
 watch(question, (newQuestions) => {
   newQuestions.forEach(q => {
@@ -128,16 +93,6 @@ const decryptId = (encryptedId) => {
   }
 };
 
-const handleSubmit = () => {
-  const nowDate = Date.now();
-  const startTimestamp = new Date(startTime.value).getTime();
-  const showTime = startTime.value.replace("T", " ").split("+")[0].split(".")[0];
-  if (nowDate - startTimestamp < 0) {
-    ElNotification.error(`问卷开始时间为 ${showTime}`);
-  } else {
-    showModal("QuestionnaireSubmit");
-  }
-};
 const getQuestionnaireView = () => {
   if (decryptedId.value) {
     useRequest(() => getUserAPI({ id: decryptedId.value as number }), {
@@ -149,7 +104,6 @@ const getQuestionnaireView = () => {
           time.value = formData.value.time.replace("T", " ").split("+")[0].split(".")[0];
           submitData.value.id = res.data.id;
           startTime.value = res.data.start_time;
-          // console.log("问卷id:"+submitData.value.id)
           question.value.forEach(q => {
             // 获取已存储的答案
             const storedAnswer = questionnaireStore.searchAnswer(decryptedId.value, q.serial_num);
@@ -177,89 +131,9 @@ const getQuestionnaireView = () => {
   }
 };
 
-const checkAnswer = () => {
-  const hasUnansweredRequiredQuestion = question.value.some((q) => {
-    if (q.required && q.answer === "") {
-      ElNotification.error("您有题目未完成作答.");
-      return true;
-    }
-    if (q.question_type === 1 && q.answer === " ") {
-      ElNotification.error("您有单选题未完成作答.");
-      return true;
-    }
-    if (q.question_type === 2 && (q.answer.endsWith("┋") || q.answer.startsWith("┋"))) {
-      ElNotification.error("您有多选题未完成作答.");
-      return true;
-    }
-    if (q.question_type === 2 && q.answer.split("┋").length > q.maximum_option || q.answer.split("┋").length < q.minimum_option) {
-      if (q.answer.split("┋").length > q.maximum_option) {
-        ElNotification.error(`该投票最多只能选择${q.maximum_option}个选项`);
-      } else if (q.answer.split("┋").length < q.minimum_option) {
-        ElNotification.error(`该投票最少需要选择${q.maximum_option}个选项`);
-      }
-      return true;
-    }
-
-    if (q.question_type === 3 && q.answer !== "" && q.reg && !new RegExp(q.reg).test(q.answer)) {
-      ElNotification.error(`第${q.serial_num}题的回答不符合要求.`);
-      return true;
-    }
-  });
-  if (hasUnansweredRequiredQuestion) {
-    showModal("QuestionnaireSubmit", true);
-    allowSend.value = false;
-    return;
-  }
-  allowSend.value = true;
-};
-
-const submit = () => {
-  checkAnswer();
-  if (allowSend.value === false) {
-    return;
-  }
-  submitData.value.questions_list = question.value.map((q) => ({
-    question_id: q.id,
-    serial_num: q.serial_num,
-    answer: q.answer
-  }));
-  submitData.value.token = localStorage.getItem("token");
-  useRequest(() => setUserSubmitAPI(submitData.value), {
-    onBefore: () => startLoading(),
-    async onSuccess(res) {
-      if (res.code === 200 && res.msg === "OK") {
-        const imageStore = useMainStore().useImageStore();
-        ElNotification.success("提交成功");
-        questionnaireStore.deleteAnswer(decryptedId.value);
-        imageStore.clearFiles();
-        optionStore.deleteOption(decryptedId.value);
-        if (formData.value.survey_type === 0) {
-          router.push("/Thank");
-        } else {
-          try {
-            const res = await getStatistic({ id: Number(decryptedId.value) });
-            resultData.value = res.data.statistics[0].options;
-          } catch (e) {
-            ElNotification.error(e);
-          }
-        }
-      } else {
-        ElNotification.error(res.msg);
-      }
-    },
-    onError(e) {
-      ElNotification.error(e.message);
-    },
-    onFinally: () => {
-      showModal("QuestionnaireSubmit", true);
-      closeLoading();
-    }
-  });
-};
-
 </script>
 
-  <style scoped>
+<style scoped>
   pre {
     white-space: pre-wrap; /* css-3 */
     word-wrap: break-word; /* InternetExplorer5.5+ */
@@ -267,4 +141,4 @@ const submit = () => {
     white-space: -o-pre-wrap; /* Opera7 */
   }
 
-  </style>
+</style>
