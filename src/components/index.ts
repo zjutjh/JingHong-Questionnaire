82dc62e4ae0a1b8018087918e010918c6d778b53
wjsds666 <2784672381@qq.com>
2025-10-30 14:24:38 +0800

    修复认证窗口时机
    feat：新增 ModalNoClose 组件并更新认证相关视图逻辑
    引入无关闭按钮的新弹窗组件 ModalNoClose
    在 view.vue 中使用 ModalNoClose 实现认证提示弹窗
    根据验证状态与 token 过期时间优化渲染逻辑
    改进用户输入与提交流程，必要时强制要求认证
    进入页面直接出现认证界面且无法退出，认证过后才能进行问卷填写
    添加 pnpm workspace 配置以高效管理依赖

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 import modal from "./Modal/index.vue";
 import { showModal } from "./Modal/showModal";
 import skeleton from "./Skeleton/index.vue";
+import ModalNoClose from "./Modal/indexNoClose.vue"; // 引入无叉号弹窗组件
 
+export { ModalNoClose }; // 导出无叉号弹窗组件及其显示函数
 export { modal, showModal, skeleton };
